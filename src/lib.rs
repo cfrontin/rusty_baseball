e5a42a0cccbba6b17c37dc1cc83ec59e691b46eb
Cory Frontin <cory.frontin@nrel.gov>
2026-05-15 13:48:12 -0600

    small update and reformat

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
 use std::fmt;
 
 pub struct BaseballState {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
+    pub x: f64, // ft, x location on an axis from 3B -> 1B centered on plate-2B line
+    pub y: f64, // ft, y location on plate-2B line starting from plate center (1B/3B line intersection)
+    pub z: f64, // ft, z location (above field level)
 }
 
 impl fmt::Display for BaseballState {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "BaseballState(x: {}, y: {}, z: {})", self.x, self.y, self.z)
+        write!(
+            f,
+            "BaseballState(x: {}, y: {}, z: {})",
+            self.x, self.y, self.z
+        )
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use rusty_baseball;
 
 fn main() {
-
-    let bbs = rusty_baseball::BaseballState{
-        x: 5.0, 
-        y: 5.0, 
-        z: 5.0, 
+    let bbs = rusty_baseball::BaseballState {
+        x: 0.0,  // ft
+        y: 55.0, // ft
+        z: 5.0,  // ft
     };
 
     println!("{}", bbs);
-
 }
